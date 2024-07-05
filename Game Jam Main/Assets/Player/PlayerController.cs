c3c07d3bcc0d485562a2fec50b21d1bce571211b
StanMaaktGames <100526568+StanMaaktGames@users.noreply.github.com>
2024-07-05 11:01:52 +0200

    player stuff

--- Game Jam Main/Assets/Player/PlayerController.cs
+++ Game Jam Main/Assets/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
@@ -12,6 +13,8 @@ public class PlayerController : MonoBehaviour
     public bool grounded = false;
     bool reversing = false;
     List<Vector3> pastPositions = new List<Vector3>();
+    Vector3 deltaPosition;
+    Vector3 previousPosition;
 
     public float moveSpeed = 1f;
     public float coyoteSeconds = 0.1f;
@@ -19,16 +22,49 @@ public class PlayerController : MonoBehaviour
     public float airDrag;
     public float reverseLength = 5f;
 
+    [Header("UI")]
+
+    public Slider reverseSlider;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        previousPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!reversing)
+        if (Input.GetKeyDown("space"))
+        {
+            reversing = true;
+        }
+        else if (Input.GetKeyUp("space"))
+        {
+            reversing = false;
+
+        }
+
+        reverseSlider.value = pastPositions.Count / (reverseLength * 50);
+        Debug.Log(pastPositions.Count);
+    }
+
+    void FixedUpdate()
+    {
+        if (reversing)
+        {
+            if (pastPositions.Count > 0)
+            {
+                transform.position = pastPositions.Last();
+                pastPositions.RemoveAt(pastPositions.Count - 1);
+            }
+            else
+            {
+                reversing = false;
+            }
+        }
+        else
         {
             velocity.x = Input.GetAxis("Horizontal") * moveSpeed;
 
@@ -39,38 +75,23 @@ public class PlayerController : MonoBehaviour
             }
 
             transform.position += velocity * Time.deltaTime;
-        }
 
-        if (Input.GetKeyDown("space"))
-        {
-            reversing = true;
-        }
-        else if (Input.GetKeyUp("space"))
-        {
-            reversing = false;
-        }
-    }
 
-    void FixedUpdate()
-    {
-        if (reversing)
-        {
-            transform.position = pastPositions.Last();
-            pastPositions.RemoveAt(pastPositions.Count - 1);
-        }
-        else
-        {
+
             pastPositions.Add(transform.position);
             if (pastPositions.Count > reverseLength * 50)
             {
                 pastPositions.RemoveAt(0);
             }
         }
+
+        deltaPosition = transform.position - previousPosition;
+        previousPosition = transform.position;
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.layer == 3)
+        if (other.gameObject.layer == 3 && deltaPosition.y < 0f)
         {
             Debug.Log("Collision enter");
             grounded = true;
